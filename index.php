5a6417073112deccb2eb6918a903920f0bf34054
Meulemans Philippe <meulemansph@hotm.com>
2021-12-17 16:28:51 +0100

    Search working

--- index.php
+++ index.php
@@ -12,7 +12,22 @@ try {
   echo "Connection failed: " . $e->getMessage();
 }
 
-$selectAll = $pdo->prepare("SELECT * from hikes ORDER BY id DESC;");
+// search bar
+$search = $_GET['search'] ?? '';
+
+if ($search) {
+  $selectAll = $pdo->prepare("SELECT * FROM hikes WHERE title LIKE :title ORDER BY id DESC;");
+  $selectAll->bindValue(':title', "%$search%");
+
+}else{
+  $selectAll = $pdo->prepare("SELECT * from hikes ORDER BY id DESC;");
+
+}
+
+
+
+
+
 $selectAll->execute();
 $hikes = $selectAll->fetchAll(PDO::FETCH_ASSOC);
 
@@ -24,60 +39,62 @@ $hikes = $selectAll->fetchAll(PDO::FETCH_ASSOC);
 
 <!doctype html>
 <html lang="en">
-  <head>
-    
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="basic.css">
-    
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-    <title>Hikes</title>
-  </head>
-  <body>
-    <h1>Hikes</h1>
-    
-    <a href="add.php" class="btn btn-success btn-sm">Add new Hike</a>
-   
-    <?php
-    foreach($hikes as $h => $hike): ?>
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="basic.css">
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+  <title>Hikes</title>
+</head>
+
+<body>
+  <h1>Hikes</h1><br>
+
+  <a href="add.php" class="btn btn-success btn-sm">Add new Hike</a>
+  <br>
+  <form>
+      <div class="input-group mb-3">
+        <input type="text" class="form-control" placeholder="Search for Hikes" value="<?php echo $search ?>" name="search"> 
+          <div class="input-group-append">
+           <button class="btn btn-outline-secondary" type="submit">Search</button>
+          </div>
+    </div>
+  </form>
+  <br>
+  <?php
+  foreach ($hikes as $h => $hike) : ?>
 
 
 
-<div class="card " style="width: 18rem;">
-    <img src="<?php echo $hike['image']?>" class="card-img-top" alt="...">
-    <div class="card-body">
-        <h6><?php echo $hike["id"];?></h6>
+    <div class="card " style="width: 18rem;">
+      <img src="<?php echo $hike['image'] ?>" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h6><?php echo $hike["id"]; ?></h6>
         <h5 class="card-title"><?php echo $hike["title"]; ?></h5>
         <p class="card-text"> Duration:<?php echo $hike["duration"]; ?> min</p>
-        <p class="card-text">Elevation gain:<?php echo $hike["elevation"];?> mètres</p>
+        <p class="card-text">Elevation gain:<?php echo $hike["elevation"]; ?> mètres</p>
         <p class="card-text">Distance :<?php echo $hike["distance"]; ?> km</p>
         <!-- edit button -->
-        <a href="edit.php?id=<?php echo $hike["id"]?>"  class="btn btn-primary btn-sm">Edit</a>
+        <a href="edit.php?id=<?php echo $hike["id"] ?>" class="btn btn-primary btn-sm">Edit</a>
         <!-- delete button POST method -->
         <form action="delete.php" method="post" style="display: inline-block">
           <input type="hidden" name="id" value="<?php echo $hike["id"]; ?>" />
-          <button type="submit"  class="btn btn-danger btn-sm">Delete</button>
+          <button type="submit" class="btn btn-danger btn-sm">Delete</button>
         </form>
-        <a href="full.php?id=<?php echo $hike["id"]?>" class="btn btn-primary btn-sm">Full description</a>
-        
+        <a href="full.php?id=<?php echo $hike["id"] ?>" class="btn btn-primary btn-sm">Full description</a>
+
+      </div>
     </div>
-</div>
 
 
 
 
-    
-    <?php endforeach; ?>
-  </body>
+
+  <?php endforeach; ?>
+</body>
+
 </html>
-
-
-
-
-
-
-
-
-
-
